ad60405fd9a5362be5ce5e12739dae238a66aa28
Karolina Kelkel <karolinakelkel@Mac.communityfibre.co.uk>
2025-07-14 21:11:13 +0100

    Configure database engine and sessionmaker

--- app/database.py
+++ app/database.py
@@ -1,3 +1,11 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./urls.db'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL,
+                       connect_args={'check_same_thread': False})
+
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
 
 Base = declarative_base()
--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from database import Base
 
 
 class URL(Base):
-    __tablename__ = "urls"
+    __tablename__ = 'urls'
 
     id = Column(Integer, primary_key=True, index=True)
     original_url = Column(String, nullable=False)
