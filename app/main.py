4d8d5c0d9989fe756b39724f25075c00fe830cc8
Karolina Kelkel <karolinakelkel@Mac.communityfibre.co.uk>
2025-07-15 15:49:39 +0100

    Add endpoints for URL shortening and redirection with unique code generation

---
+++ app/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.responses import RedirectResponse
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+import models, schemas, utils
+from database import SessionLocal, engine, Base
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post('/shorten', response_model=schemas.URLResponse)
+def create_short_url(request: schemas.URLRequest, db: Session = Depends(get_db)):
+    #Collision check
+    while True:
+        short_code = utils.generate_code()
+        statement = select(models.URL).where(models.URL.short_code == short_code)
+        if db.execute(statement).scalar_one_or_none() is None:
+            break
+
+    url = models.URL(original_url=request.url, short_code=short_code)
+    db.add(url)
+    db.commit()
+
+    short_url = f'http://localhost:8000/{short_code}'
+    return schemas.URLResponse(short_url=short_url)
+
+@app.get('/{code}')
+def redirect_to_url(code: str, db: Session = Depends(get_db)):
+    statement = select(models.URL).where(models.URL.short_code == code)
+    result = db.execute(statement).scalar_one_or_none()
+    if result is None:
+        raise HTTPException(status_code=404, detail='URL not found')
+    return RedirectResponse(result.original_url)
