27a41684be17e013fb87861da2e2bcf9097e1998
Karolina Kelkel <karolinakelkel@Mac.communityfibre.co.uk>
2025-07-16 21:55:59 +0100

    Cosmetic changes

--- app/schemas.py
+++ app/schemas.py
@@ -4,5 +4,7 @@ from pydantic import BaseModel, HttpUrl, AnyHttpUrl
 class URLRequest(BaseModel):
     url: HttpUrl
 
+
 class URLResponse(BaseModel):
     short_url: AnyHttpUrl
+   
--- app/utils.py
+++ app/utils.py
@@ -4,9 +4,5 @@ from random import choices
 
 def generate_code(length: int = 5):
     chars = string.ascii_letters + string.digits + '-_'
-    print(1/64 ** 5)
-
 
     return ''.join(choices(chars, k=length))
-
-generate_code()
